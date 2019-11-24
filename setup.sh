4c06f6d7af2a33acdc8bfdb81c709f4178e680d7
Marvin Isaac <marvin@marvinisaac.com>
2019-11-24 17:19:06 +0800

    :hammer: Use echo + read to wait for user input

--- setup.sh
+++ setup.sh
@@ -11,7 +11,8 @@ setup_user () {
 
     printf "> (A) Type username to use in script, or\n"
     printf "> (B) press enter to run as \"$user\".\n"
-    read -e ">> Waiting for input: " username
+    echo ">> Waiting for input: "
+    read username
     if [ "$username" != "" ]
     then
         user=$username
@@ -101,7 +102,8 @@ setup_keys () {
     printf "> Setting up SSH keys...\n"
     wait
 
-    read -e ">> Enter SSH key identifier: " identifier
+    echo ">> Enter SSH key identifier: "
+    read identifier
     ssh-keygen -b 4096 -t rsa -C "$identifier"
 
     printf ">>> SSH keys setup.\n"
@@ -116,8 +118,10 @@ setup_static_ip () {
     printf "> Setting up static IP address...\n"
     wait
 
-    read -e">> Enter IP address to use: " ip
-    read -e ">> Enter gateway IP address: " gateway
+    echo ">> Enter IP address to use: "
+    read ip
+    echo ">> Enter gateway IP address: "
+    read gateway
     filename="/etc/netplan/50-cloud-init.yaml"
     cat <<EOF >> $filename
 network:
