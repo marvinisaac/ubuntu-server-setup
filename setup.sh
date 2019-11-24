2bc71c94646ddfe8bb036ee2099df8578cc660f2
Marvin Isaac <marvin@marvinisaac.com>
2019-11-24 11:19:19 +0800

    :hammer: Fix removal of older custom commands

--- setup.sh
+++ setup.sh
@@ -6,62 +6,62 @@ wait () {
 }
 
 update () {
-    wait
     printf "> Updating system...\n"
-    
     wait
+    
     printf "> Getting updates...\n"
+    wait
     sudo apt update -y
     
-    wait
     printf "> Installing updates...\n"
+    wait
     sudo apt upgrade -y
     
-    wait
     printf ">>> Updates installed.\n"
+    wait
 }
 
 install () {
-    wait
     printf "> Installing LEMP stack...\n"
+    wait
     sudo apt install mariadb-server nginx php-fpm -y
     
-    wait
     printf ">>> LEMP installed.\n"
-    
     wait
+    
     printf "> Installing required packages...\n"
+    wait
     sudo apt install curl git php-cli php-curl php-mbstring php-mysql php-xml unzip -y
     
-    wait
     printf "> Required packages installed.\n"
-    
     wait
-    printf "> Installing node and npm...\n"
+    
+    printf "> Installing Node and npm...\n"
+    wait
     sudo apt install nodejs npm -y
     
-    wait
     printf ">>> Node and npm installed.\n"
-    
     wait
-    printf "> Installing composer...\n"
+    
+    printf "> Installing Composer...\n"
+    wait
     curl https://getcomposer.org/installer -o composer-setup.php
     sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
     rm composer-setup.php
     
-    wait
     printf ">>> Composer installed.\n"
-    
     wait
+    
     printf ">>> Installations complete.\n"
+    wait
 }
 
 alias () {
-    wait
     printf "> Setting up custom commands...\n"
+    wait
     
     # Remove custom commands
-    sed '/^marvin-/d' ~/.bashrc
+    sed '/alias marvin/d' ~/.bashrc -in
 
     # Write aliases to ~/.bashrc
     echo "alias marvin-status=\"htop\"" >> ~/.bashrc
@@ -70,9 +70,8 @@ alias () {
     # Reload terminal to make aliases usable
     source ~/.bashrc
     
-    wait
     printf ">>> Custom commands online.\n"
-
+    wait
 }
 
 update
@@ -81,7 +80,7 @@ alias
 
 printf "   _____      __                                            __     __     \n";
 printf "  / ___/___  / /___  ______     _________  ____ ___  ____  / /__  / /____ \n";
-printf "  \__ \/ _ \/ __/ / / / __ \   / ___/ __ \/ __ \`__ \/ __ \/ / _ \/ __/ _ \\\n";
+printf "  \__ \/ _ \/ __/ / / / __ \   / ___/ __ \/ __ \`__ \/ __ \/ / _ \/ __/ _ \\n";
 printf " ___/ /  __/ /_/ /_/ / /_/ /  / /__/ /_/ / / / / / / /_/ / /  __/ /_/  __/\n";
 printf "/____/\___/\__/\__,_/ .___/   \___/\____/_/ /_/ /_/ .___/_/\___/\__/\___/ \n";
 printf "                   /_/                           /_/                      \n";
